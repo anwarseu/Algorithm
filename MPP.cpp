8e100782b69aaba88dac04ddc1745ad5304a3c78
Md Anwar Hossain <anwarseu@gmail.com>
2018-11-23 11:23:56 +0600

    Maximum Pairwise Product
    Maximum Pairwise Product Problem
    Find the maximum product of two distinct num- bers in a sequence of non-negative integers.
    Input: A sequence of non-negative integers.
    Output: The maximum value that can be obtained by multiplying two different elements from the se- quence.

---
+++ MPP.cpp
@@ -0,0 +1,41 @@
+//
+//  main.cpp
+//  MPP
+//
+//  Created by Md Anwar Hossain on 11/23/18.
+//  Copyright © 2018 Md Anwar Hossain. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::cin;
+using std::cout;
+using std:: max;
+
+int MaxPairwiseProduct(const vector<int>& numbers){
+    int product = 0;
+    int n = numbers.size();
+    
+    for (int i = 0 ; i < n; ++i) {
+        for (int j = 1; j < n; ++j) {
+            product = max(product, numbers[i]*numbers[j]);
+        }
+    }
+    return product;
+}
+int main(int argc, const char * argv[]) {
+    int n;
+    cout << "Enter your number size : ";
+    cin >> n;
+    vector<int> numbers(n);
+    
+    for (int i = 0; i < n; i++) {
+        cin >> numbers[i];
+    }
+    
+    int product = MaxPairwiseProduct(numbers);
+    cout << product << "\n";
+    return 0;
+}
